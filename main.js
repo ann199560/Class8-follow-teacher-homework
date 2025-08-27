fd43403ee78bea8a145269312eb7c2e7ec3c3785
Der Yu Tu <ann199560@gmail.com>
2025-08-27 23:49:49 +0800

    更新靈感地圖swiper輪播套件

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+
+ const swiper = new Swiper('.swiper', {
+
+   // 分頁、左右箭頭、滾動條若有使用則必需設定          
+   // 分頁
+   loop: true,
+   slidesPerView: "auto",//     //每個 slide 自己決定寬度//
+   spaceBetween: 16,
+   pagination: {
+     el: '.swiper-pagination',  //指定分頁器的 DOM 容器//
+     type: 'bullets',           //分頁器用「小圓點顯示」//
+     clickable: true,           //允許使用者直接點擊圓點切換//
+   },
+   // 左右箭頭    
+   navigation: {
+     nextEl: '.swiper-btn-next',
+     prevEl: '.swiper-btn-prev',
+   },
+
+   breakpoints: {
+    992: {
+        slidesOffsetBefore: 312, //左邊空白//
+        spaceBetween: 24,
+    }
+   }
+ });
